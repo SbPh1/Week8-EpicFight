8bf43e84e98ed667402abb514e98fed55ff36212
H <sikkelh@gmail.com>
2021-10-26 13:37:27 +0300

    Functions with returns

--- EpicFight/Program.cs
+++ EpicFight/Program.cs
@@ -12,10 +12,48 @@ namespace EpicFight
             heroWeapon = RandomWeapon();
             villain = RandomVillain();
             villainWeapon = RandomWeapon();
+            int heroHP = GenerateHP(hero);
+            int villainHP = GenerateHP(villain);
 
-            Console.WriteLine($"{hero} will fight {villain} with a {heroWeapon}, while {villain} fights back with a {villainWeapon}.");
-            
+            Console.WriteLine($"{hero} ({heroHP}HP) will fight {villain} ({villainHP}HP) with a {heroWeapon}, while {villain} fights back with a {villainWeapon}.");
+            while (heroHP > 0 && villainHP > 0)
+            {
+                heroHP = heroHP - Hit(villain, hero, villainWeapon);
+                villainHP = villainHP - Hit(hero, villain, heroWeapon);
+            }
+            if (heroHP <= 0)
+            {
+                Console.WriteLine("Dark side wins!");
+            }
+            else
+            {
+                Console.WriteLine($"{hero} saves the day!");
+            }
 
+        }
+
+        private static int Hit(string characterOne, string characterTwo, string weapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, weapon.Length / 2);
+            Console.WriteLine($"{characterOne} hit {strike}.");
+
+            if (strike == weapon.Length/2 - 1)
+            {
+                Console.WriteLine($"Awesome! {characterOne} made a critical hit");
+            }
+            else if (strike == 0)
+            {
+                Console.WriteLine($"{characterTwo} dodged the attack!");
+            }
+
+            return strike;
+        }
+
+        private static int GenerateHP(string someCharacter)
+        {
+            Random rnd = new Random();
+            return rnd.Next(someCharacter.Length, someCharacter.Length + 10);
         }
 
         private static int RandomIndex(string[] someArray)
